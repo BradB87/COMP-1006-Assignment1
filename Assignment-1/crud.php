eed38f66d64addbbec63754f83e4a8d69eb205ea
Brad Buttineau <113149072+BradB87@users.noreply.github.com>
2023-10-06 18:30:57 -0400

    commit 4 change of php

---
+++ Assignment-1/crud.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'database.php';
+//This class is an extension of our database class and will execute out database queries.
+class crud extends database
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function getData()
+    {
+        $result = $this->connection->query($query);
+        if ($result == false) {
+            return false;
+        }
+        $rows = array();
+        while ($row = $result->fetch_assoc()) {
+            $rows[] = $row;
+        }
+        return $rows;
+    }
+    public function execute($query)
+    {
+        $result = $this->connection->query($query);
+        if ($result == false) {
+            echo "Error cannot execute the command";
+            return false;
+        } else {
+            return true;
+        }
+    }
+    public function escape_string($value)
+    {
+        return $this->connection->real_escape_string($value);
+    }
+}
---
+++ Assignment-1/view.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="view.css">
+	<title>Assignment 1</title>
+</head>
+
+<body>
+
+	<div class="container">
+		<div class="row">
+			<?php
+			include_once 'crud.php';
+			$crud = new crud();
+			$query = 'SELECT * FROM Assignment1';
+			$result = $crud->getData($query);
+			?>
+			<table class="table">
+				<tr>
+					<th>First Name</th>
+					<th>Last Name</th>
+					<th>Address</th>
+					<th>Mobile</th>
+					<th>Email</th>
+					<th>Payment</th>
+				</tr>
+				<?php
+
+				foreach ($result as $key => $res) {
+					echo "<tr>";
+					echo "<td>" . $res['fname'] . "</td>";
+					echo "<td>" . $res['lname'] . "</td>";
+					echo "<td>" . $res['address'] . "</td>";
+					echo "<td>" . $res['mobile'] . "</td>";
+					echo "<td>" . $res['email'] . "</td>";
+					echo "<td>" . $res['payment'] . "</td>";
+					echo "</tr>";
+				}
+			
+				?>
+
+			</table>
+		</div>
+	</div>
+
+</body>
+
+</html>
